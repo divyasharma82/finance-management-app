703bcb3130fb8d6ec333fa7f7ffe58759e373982
Divya Sharma <122516735+divyasharma82@users.noreply.github.com>
2024-05-24 10:09:07 +0530

    solve the firstName error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,35 +5,35 @@ import React from 'react';
 
 const Footer = ({ user, type = 'desktop' }: FooterProps) => {
   const router = useRouter();
-  const handleLogoutFunction = async () => {
+
+  const handleLogOut = async () => {
     const loggedOut = await logoutAccount();
-    if (loggedOut) {
-      router.push('/sign-in');
-    }
+
+    if (loggedOut) router.push('/sign-in');
   };
+
+  const firstNameInitial = user?.name ? user.name.charAt(0) : '';
   return (
-    <div>
-      <footer className="footer">
-        <div
-          className={type === 'mobile' ? 'footer_name_mobile' : 'footer_name'}
-        >
-          <p className="text-xl font-bold text-gray-700">{user?.name[0]}</p>
-        </div>
-        <div
-          className={type === 'mobile' ? 'footer_email_mobile' : 'footer_email'}
-        >
-          <h1 className="text-14 truncate  text-gray-600 font-semibold">
-            {user?.name}
-          </h1>
-          <p className="text-14 truncate font-normal text-gray-600">
-            {user?.email}
-          </p>
-        </div>
-        <div className="footer_image" onClick={handleLogoutFunction}>
-          <Image src="icons/logout.svg" fill alt="logout-logo" />
-        </div>
-      </footer>
-    </div>
+    <footer className="footer">
+      <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
+        <p className="text-xl font-bold text-gray-700">{firstNameInitial}</p>
+      </div>
+
+      <div
+        className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}
+      >
+        <h1 className="text-14 truncate text-gray-700 font-semibold">
+          {user?.name}
+        </h1>
+        <p className="text-14 truncate font-normal text-gray-600">
+          {user?.email}
+        </p>
+      </div>
+
+      <div className="footer_image" onClick={handleLogOut}>
+        <Image src="icons/logout.svg" fill alt="jsm" />
+      </div>
+    </footer>
   );
 };
 
