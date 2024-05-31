351b4bad6f82a7675dc5b5fde70b1242771d4514
Divya Sharma <122516735+divyasharma82@users.noreply.github.com>
2024-05-31 17:57:19 +0530

    implement bank fetch functionality using dwolla

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import React from 'react';
 
 const Home = async () => {
   const loggedIn = await getLoggedInUser();
+  
+
   return (
     <section className="home">
       <div className="home-content">
